8b5a972ba1e22f8abf1b0bbd409bf80b8fdc5b9b
анастасия <anastasia@MacBook-Air-anastasia.local>
2024-12-10 17:29:13 +0300

    test-api

--- internal/handlers/handler.go
+++ internal/handlers/handler.go
@@ -5,7 +5,3 @@ import "github.com/julienschmidt/httprouter"
 type Handler interface {
 	Register(router *httprouter.Router)
 }
-
-func handler() {
-
-}
--- internal/user/handler.go
+++ internal/user/handler.go
@@ -2,9 +2,12 @@ package user
 
 import (
 	"net/http"
+	"rest-api/internal/handlers"
 
 	"github.com/julienschmidt/httprouter"
 )
+
+var _ handlers.Handler = &handler{}
 
 const (
 	usersURL = "/users"
@@ -14,9 +17,13 @@ const (
 type handler struct {
 }
 
+func NewHandler() handlers.Handler {
+	return &handler{}
+}
+
 func (h *handler) Register(router *httprouter.Router) {
-	router.GET(userURL, h.GetList)
-	router.POST(userURL, h.CreateUser)
+	router.GET(usersURL, h.GetList)
+	router.POST(usersURL, h.CreateUser)
 	router.GET(userURL, h.GetUserByID)
 	router.PUT(userURL, h.UpdateUser)
 	router.PATCH(userURL, h.PartionallyUpdateUser)
@@ -24,22 +31,34 @@ func (h *handler) Register(router *httprouter.Router) {
 }
 
 func (h *handler) GetList(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(200)
 	w.Write([]byte("this is list of users"))
+
 }
 
 func (h *handler) CreateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-	w.Write([]byte("this is create user"))
+	w.WriteHeader(201)
+	w.Write([]byte("this is list of users"))
+
 }
 
 func (h *handler) GetUserByID(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(200)
 	w.Write([]byte("this is user by uuid"))
+
 }
 func (h *handler) UpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(204)
 	w.Write([]byte("this is update user"))
+
 }
 func (h *handler) PartionallyUpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(204)
 	w.Write([]byte("this is partionally update user"))
+
 }
 func (h *handler) DeleteUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(204)
 	w.Write([]byte("this is delete user"))
+
 }
