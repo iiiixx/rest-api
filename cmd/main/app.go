8538f8d23c1dc78a8ff602728ebfc3564c9d4f0a
анастасия <anastasia@MacBook-Air-anastasia.local>
2024-12-10 17:26:09 +0300

    handler, http request, func start

--- cmd/main/app.go
+++ cmd/main/app.go
@@ -7,9 +7,12 @@ import (
 	"net/http"
 	"time"
 
+	"rest-api/internal/user"
+
 	"github.com/julienschmidt/httprouter"
 )
 
+// IndexHandler — обработчик запросов
 func IndexHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	name := params.ByName("name")
 	w.Write([]byte(fmt.Sprintf("Hello %s", name)))
@@ -17,19 +20,33 @@ func IndexHandler(w http.ResponseWriter, r *http.Request, params httprouter.Para
 }
 
 func main() {
+	log.Println("create router")
 	router := httprouter.New()
-	router.GET("/:name", IndexHandler)
+
+	log.Println("register user handler")
+	handler := user.NewHandler()
+	handler.Register(router)
+
+	start(router)
+
+}
+
+// Настройка и запуск сервера
+func start(router *httprouter.Router) {
+	log.Println("start application")
 
 	listener, err := net.Listen("tcp", ":1234")
 	if err != nil {
 		panic(err)
 	}
 
+	//Настройка таймаутов и запуск HTTP-сервера
 	server := &http.Server{
 		Handler:      router,
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
-	log.Fatalln(server.Serve(listener))
 
+	log.Println("server is listening port 0.0.0.0:1234")
+	log.Fatalln(server.Serve(listener))
 }
